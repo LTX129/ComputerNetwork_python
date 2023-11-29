3079985208fef7202433fc3cd603cdd4cb1693df
Li <21722052@bjtu.edu.cn>
2023-11-29 09:32:11 +0800

    将Traceroute代码格式修改为同Ping基本相似

--- Traceroute.py
+++ Traceroute.py
@@ -43,53 +43,67 @@ def create_packet(id):
     header = struct.pack('bbHHh', 8, 0, my_checksum, id, 1)
     return header + data.encode('utf-8')
 
-def traceroute(host, max_hops=50, timeout=2):
-    """trace
-    Run the traceroute to the given destination address (dest_addr).
+def sendOnePing(send_socket, dest_addr, ID):
     """
-    dest_addr = socket.gethostbyname(host)
+    Send one ping to the destination address using the given socket.
+    """
+    packet = create_packet(ID)
+    send_socket.sendto(packet, (dest_addr, 1))
+
+def receiveOnePing(recv_socket, ID, timeout, ttl):
+    """
+    Receive the ping from the socket.
+    """
+    # Timeout for the socket to wait for a reply
+    recv_socket.settimeout(timeout)
+
+    try:
+        started_select = time.time()
+        what_ready = select.select([recv_socket], [], [], timeout)
+        how_long_in_select = (time.time() - started_select)
+        if what_ready[0] == []:  # Timeout
+            return ttl, None, '*'
+        recv_packet, addr = recv_socket.recvfrom(1024)
+        return ttl, addr[0], how_long_in_select
+    except socket.timeout:
+        return ttl, None, '*'
+
+
+def doOnePing(dest_addr, ttl, timeout):
+    """
+    Perform one ping operation.
+    """
     icmp_proto = socket.getprotobyname('icmp')
-    ttl = 1
-    print(f"通过最多 {max_hops} 个跃点跟踪")
-    # print(f"到 {dest_addr} 的路由:\n")
-    print("到 " + host + " (" + dest_addr + ") 的路由 using Python:")
-
-
-    while True:
-        recv_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp_proto)
-        send_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp_proto)
-
+    with socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp_proto) as send_socket, \
+            socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp_proto) as recv_socket:
         send_socket.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, struct.pack('I', ttl))
-        # Timeout for the socket to wait for a reply
-        recv_socket.settimeout(timeout)
         recv_socket.bind(("", 0))
 
-        id = os.getpid() & 0xFFFF
+        ID = os.getpid() & 0xFFFF
 
-        packet = create_packet(id)
-        send_socket.sendto(packet, (dest_addr, 1))
+        sendOnePing(send_socket, dest_addr, ID)
+        return receiveOnePing(recv_socket, ID, timeout, ttl)
 
-        try:
-            started_select = time.time()
-            what_ready = select.select([recv_socket], [], [], timeout)
-            how_long_in_select = (time.time() - started_select)
-            if what_ready[0] == []:  # Timeout
-                print(f"{ttl:4}   *        *        *     请求超时。")
-            recv_packet, addr = recv_socket.recvfrom(1024)
-            time_received = time.time()
-            time_passed = round((time_received - started_select) * 1000)
-            print(f"{ttl:4}  {time_passed} ms    {time_passed} ms    {time_passed} ms  {addr[0]}")
-        except socket.timeout:  # No reply within timeout
+
+def traceroute(host, max_hops=50, timeout=2):
+    """
+    Run the traceroute to the given host.
+    """
+    dest_addr = socket.gethostbyname(host)
+    print(f"通过最多 {max_hops} 个跃点跟踪到 {host} ({dest_addr}):")
+
+    for ttl in range(1, max_hops + 1):
+        ttl, addr, time_passed = doOnePing(dest_addr, ttl, timeout)
+        if addr:
+            print(f"{ttl:4}  {time_passed * 1000:.0f} ms    {addr}")
+        else:
             print(f"{ttl:4}   *        *        *     请求超时。")
-        finally:
-            send_socket.close()
-            recv_socket.close()
 
-        ttl += 1
-        if addr[0] == socket.gethostbyname(dest_addr) or ttl > max_hops:
+        if addr == dest_addr:
             break
 
     print("\n跟踪完成。")
 
+
 # Example usage
-traceroute("lancaster.ac.uk")
+traceroute("www.baidu.com")
