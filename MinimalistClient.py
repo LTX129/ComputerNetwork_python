c1c8e19757ad1c9759a12049fe3057a507b7288d
Li <21722052@bjtu.edu.cn>
2023-12-08 22:14:07 +0800

    修改了客户端的中英文

--- MinimalistClient.py
+++ MinimalistClient.py
@@ -8,22 +8,22 @@ def fetch_file_and_print(url):
 
         # 检查响应状态码
         if response.status_code == 200:
-            print("HTTP 响应信息:")
-            print(f"状态码: {response.status_code}")
-            print(f"响应头:\n{response.headers}\n")
+            print("HTTP response message:")
+            print(f"status code: {response.status_code}")
+            print(f"Response header:\n{response.headers}\n")
 
             # 获取文件内容并打印
             file_content = response.text
-            print("文件内容:")
+            print("file content:")
             print(file_content)
         else:
-            print(f"HTTP 请求失败，状态码: {response.status_code}")
+            print(f"HTTP request failed, status code: {response.status_code}")
 
     except requests.exceptions.RequestException as e:
-        print(f"发生请求异常: {e}")
+        print(f"Request exception occurred: {e}")
 
 
 if __name__ == "__main__":
     # 获取用户输入的文件URL
-    file_url = input("请输入文件的URL地址: ")
+    file_url = input("Please enter the URL of the file: ")
     fetch_file_and_print(file_url)
