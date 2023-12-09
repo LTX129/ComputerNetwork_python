6beae42673473530cbaba6360c54ea2f56a04482
Li <21722052@bjtu.edu.cn>
2023-12-09 20:57:11 +0800

    解耦合，将GET,PUT,DELETE等操作单独拿出来；可以正常进行http1.1的PUT和DELETE操作；进行了python格式规范的修改

--- WebServer.py
+++ WebServer.py
@@ -1,10 +1,16 @@
-
 import socket
 import os
 import threading
+import mimetypes
 
 N = None
-def create_server_socket(addr,port):
+error_500 = "500 Internal Server Error"
+error_404 = '404 Not Found'
+error_400 = '400 Bad Request'
+error_501 = '501 Not Implemented'
+
+
+def create_server_socket(addr, port):
     # Create a socket and bind to the specified port
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((addr, port))
@@ -12,7 +18,8 @@ def create_server_socket(addr,port):
     print(f"Web server running on port {port}")
     return server_socket
 
-def handle_request(client_socket, addr):
+
+def handle_request(client_socket):
     # Handle the incoming client request
     try:
         request = client_socket.recv(4096).decode()
@@ -22,36 +29,20 @@ def handle_request(client_socket, addr):
         else:
             print("Received empty request")
             return
+        request_type, filename = extract_request_type_and_filename(request_lines[0])
 
-        filename = extract_file_name(request_lines[0])
-
-        if filename:
-            try:
-                with open(filename, 'rb') as file:
-                    file_content = file.read()
-                    response_header = 'HTTP/1.1 200 OK\r\n'
-                    response_header += 'Content-Type: text/html; charset=utf-8\r\n'
-                    response_header += f'Content-Length: {len(file_content)}\r\n\r\n'
-                    response = response_header.encode() + file_content
-            except FileNotFoundError:
-                response_header = 'HTTP/1.1 404 Not Found\r\n\r\n'
-                response_body = 'File not found'
-                response = response_header.encode() + response_body.encode()
+        if request_type == 'GET':
+            handle_get_request(client_socket, filename)
+        elif request_type == 'PUT':
+            handle_put_request(client_socket, filename, request)
+        elif request_type == 'DELETE':
+            handle_delete_request(client_socket, filename)
         else:
-            response_header = 'HTTP/1.1 400 Bad Request\r\n\r\n'
-            response_body = 'Bad Request'
-            response = response_header.encode() + response_body.encode()
+            send_error_response(client_socket, error_501)
 
-        client_socket.sendall(response)
     finally:
         print("client close")
         client_socket.close()
-
-def extract_file_name(request_line):
-    # Extract the file name from the HTTP GET request
-    parts = request_line.split()
-    if len(parts) > 1 and parts[0] == 'GET':
-        return parts[1].strip('/')
 
 
 def handle_get_request(client_socket, filename):
@@ -59,15 +50,31 @@ def handle_get_request(client_socket, filename):
         try:
             with open(filename, 'rb') as file:
                 file_content = file.read()
-                response_header = 'HTTP/1.1 200 OK\r\n'
-                response_header += 'Content-Type: text/html; charset=utf-8\r\n'
+                # 使用mimetypes库获取文件扩展名对应的MIME类型
+                mime_type, _ = mimetypes.guess_type(filename)
+                if mime_type is None:
+                    mime_type = 'application/octet-stream'  # 默认的二进制流类型
+
+                response_header = "HTTP/1.1 200 OK\r\n"
+                response_header += f'Content-Type: {mime_type}\r\n'
                 response_header += f'Content-Length: {len(file_content)}\r\n\r\n'
+
                 response = response_header.encode() + file_content
-                client_socket.sendall(response)
         except FileNotFoundError:
-            send_error_response(client_socket, '404 Not Found')
+            response_header = 'HTTP/1.1 404 Not Found\r\n\r\n'
+            response_body = 'File not found'
+            response = response_header.encode() + response_body.encode()
+            send_error_response(client_socket, error_404)
+        except Exception:
+            send_error_response(client_socket, error_500)
     else:
-        send_error_response(client_socket, '400 Bad Request')
+        response_header = 'HTTP/1.1 400 Bad Request\r\n\r\n'
+        response_body = 'Bad Request'
+        response = response_header.encode() + response_body.encode()
+        send_error_response(client_socket, error_400)
+
+    client_socket.sendall(response)
+
 
 def handle_put_request(client_socket, filename, request):
     try:
@@ -76,8 +83,9 @@ def handle_put_request(client_socket, filename, request):
             file.write(content.encode())
         response_header = 'HTTP/1.1 201 Created\r\n\r\n'
         client_socket.sendall(response_header.encode())
-    except Exception as e:
-        send_error_response(client_socket, '500 Internal Server Error')
+    except Exception:
+        send_error_response(client_socket, error_500)
+
 
 def handle_delete_request(client_socket, filename):
     try:
@@ -85,24 +93,29 @@ def handle_delete_request(client_socket, filename):
         response_header = 'HTTP/1.1 200 OK\r\n\r\n'
         client_socket.sendall(response_header.encode())
     except FileNotFoundError:
-        send_error_response(client_socket, '404 Not Found')
-    except Exception as e:
-        send_error_response(client_socket, '500 Internal Server Error')
+        send_error_response(client_socket, error_404)
+    except Exception:
+        send_error_response(client_socket, error_500)
+
 
 def send_error_response(client_socket, error_message):
     # Function to send an error response
     response_header = f'HTTP/1.1 {error_message}\r\n\r\n'
     client_socket.sendall(response_header.encode())
 
+
 def extract_request_type_and_filename(request_line):
+    # Extract the file name from the HTTP request
     parts = request_line.split()
     if len(parts) > 1:
         return parts[0], parts[1].strip('/')
     return None, None
 
-def startServer(serveraddr, port):
+
+def start_server(server_addr, port):
+    server_socket = None
     try:
-        server_socket = create_server_socket(serveraddr, port)
+        server_socket = create_server_socket(server_addr, port)
         while True:
             client_socket, addr = server_socket.accept()
             print(f"one connection is established and it's address is: {addr}")
@@ -114,6 +127,7 @@ def startServer(serveraddr, port):
     finally:
         server_socket.close()
 
+
 if __name__ == "__main__":
     port_input = int(input("Please enter the port of the server: "))
-    startServer("", port_input)
+    start_server("", port_input)
